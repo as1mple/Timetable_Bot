bd9e95b8340c00f1e6fe2608729d018b155215b9
asimple <netron61@gmail.com>
2020-11-01 14:37:51 +0200

    added calendar, connecting with backend, subgroup, validation

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import config
-from connect import get_response
+from connect import *
 
 import logging
 
@@ -7,9 +7,9 @@ import telebot
 
 from telebot import types
 
-DAYS = {'Пн': 0, 'Вт': 1, 'Ср': 2, 'Чт': 3, 'Пт': 4, 'Сб': 5, 'Нд': 6}
 CS = {'I': 1, 'II': 2, 'III': 3, 'IV': 4}
 info = {}
+faculty = []
 
 # Запускает т.н. Long Polling, а параметр none_stop=True говорит,
 # что бот должен стараться не прекращать работу при возникновении каких-либо ошибок.
@@ -24,138 +24,168 @@ logging.info("Bot в режиме ожидания")
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
+    """Обработчик  команд"""
     logging.info(f"Отправлена команда => {message.text}")
     if message.text == "/start":
         logging.info(f"Бот был запущен полтзователем {message.chat.id}")
         bot.send_message(message.chat.id, 'Щоб розпочати роботу бота привітайтесь з ним :=)')
         bot.register_next_step_handler(message, say_hello)
     elif message.text == '/help':
-        bot.send_message(message.chat.id, 'Тут має щось бути...')
+        bot.send_message(message.chat.id,
+                         'Даний бот був розрозроблений з ціллю полегшити пошук енного розкладу в енний день енного місяця для користувачів сайту ВНТУ - iq.vntu.edu.ua ')
+
+        bot.send_message(message.chat.id,
+                         'Для того, щоб розпочати пошук розкладу по вашим критерiям: \n'
+                         '=> Вам потрiбно  привітатись з ботом'
+                         'на українській, англійській або російській мовах \n=> після чого вводити коректну інформація на вимоги бота.\n'
+                         '!!! Якщо інформація не буде схожа на правду вам запропонується повторити спробу. '
+                         '!!! Розклад по вашим критеріям не буде знайдений, якщо: \n'
+                         '=> На частині бекенду будуть проводитись технічні роботи, \n'
+                         '=> Сайт який надає розклад буде тимчасово недоступний,\n'
+                         '=> Ваша інформація була схожа на правдиву - '
+                         'вам вдалося обдурити валідатор бота =-)')
 
 
 @bot.message_handler(func=lambda message: True, content_types=["text"])
 def event_handler(message):
+    """Главный обработчик событий"""
     logging.info(f"сообщение которое пришло => {message.text}")
 
     if word_search(message.text):
         say_hello(message)
-    elif message.text == 'ТАК':
+    elif message.text.upper() == 'ТАК':
         markup = types.ReplyKeyboardMarkup()
-        markup.row('Ця неділя', 'Наступна неділя')
-        bot.send_message(message.chat.id,
-                         "Вас цікавить розклад на цій неді чи на наступній (діапазон буде розширений у наступних релізах",
-                         reply_markup=markup)
-        bot.register_next_step_handler(message, get_day)
+        month = [str(i) for i in range(1, 13)]
+        markup.row(*month[:3])
+        markup.row(*month[3:6])
+        markup.row(*month[6:9])
+        markup.row(*month[9:])
+
+        bot.send_message(message.chat.id, "Виберіть місяць в яку вас цікавить розклад", reply_markup=markup)
+        bot.register_next_step_handler(message, get_month)
     else:
-        bot.send_message(message.chat.id, f"Не зрозумів. Перевірте правильність введення")
+        markup = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, f"Захочете знайти розкад, просто привітайтесь зі мною", reply_markup=markup)
 
 
 def say_hello(message):
     """Данная функция является первой в цепочке обработке событий не считая /start"""
     if word_search(message.text):
         logging.info("С ботом поздоровались")
+        global info
+        info.clear()
         bot.send_message(message.chat.id,
                          "Доброго часу доби, вас вітає бот для пошуку розкладу.")
         markup = types.ReplyKeyboardMarkup()
         markup.row('I', 'II')
         markup.row('III', 'IV')
-        bot.send_message(message.chat.id, "Виберіть курс групи, яка вас цікавить", reply_markup=markup)
+        bot.send_message(message.chat.id, "Виберіть або введіть курс групи, яка вас цікавить", reply_markup=markup)
         bot.register_next_step_handler(message, get_course)
 
 
 def get_course(message):
-    course = CS[message.text]
-    save_data('course', course)
-    markup = types.ReplyKeyboardMarkup()
+    if not message.text in [str(i) for i in range(1, 5)] and CS.get(message.text, 'ERROR') is "ERROR":
+        bot.send_message(message.chat.id, "Не коректнi данi. Повторiть спробу!")
+        bot.send_message(message.chat.id, "Виберіть або введіть курс групи, яка вас цікавить")
+        bot.register_next_step_handler(message, get_course)
+    else:
+        course = CS.get(message.text, message.text)
 
-    answer = get_response(params=info)['message']
+        save_data('course', course)
+        markup = types.ReplyKeyboardMarkup()
 
-    at = answer.find('[') + 1
-    to = answer.find(']')
-    [markup.row(el) for el in answer[at:to].split(',')]
+        answer = get_timetable(params=info)['message']
 
-    bot.send_message(message.chat.id, f"Ітак ви на {course} курсі. Вкажіть назву вашого факультету",
-                     reply_markup=markup)
+        at = answer.find('[') + 1
+        to = answer.find(']')
+        global faculty
+        faculty = answer[at:to].split(', ')
+        [markup.row(el) for el in faculty]
 
-    bot.register_next_step_handler(message, get_fac)
+        bot.send_message(message.chat.id, f"Ітак ви на {course} курсі. Виберіть назву вашого факультету",
+                         reply_markup=markup)
+
+        bot.register_next_step_handler(message, get_fac)
 
 
 def get_fac(message):
-    fac = message.text
-    save_data('fac', fac)
-    markup = types.ReplyKeyboardRemove()
-    bot.send_message(message.chat.id, f"Напишіть назву вашої групи", reply_markup=markup)
-    bot.register_next_step_handler(message, get_week)
+    if not message.text in faculty:
+        bot.send_message(message.chat.id, "Не коректнi данi. Повторiть спробу!")
+        bot.send_message(message.chat.id, f"Виберіть назву вашого факультету")
+        bot.register_next_step_handler(message, get_fac)
+    else:
+        fac = message.text
+        save_data('fac', fac)
+        markup = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, f"Напишіть назву вашої групи", reply_markup=markup)
+        bot.register_next_step_handler(message, get_group)
 
 
-def get_week(message):
+def get_group(message):
     group = message.text
     save_data('group', group)
+
     markup = types.ReplyKeyboardMarkup()
-    markup.row('Ця неділя', 'Наступна неділя')
-    bot.send_message(message.chat.id,
-                     "Вас цікавить розклад на цій неді чи на наступній (діапазон буде розширений у наступних релізах",
-                     reply_markup=markup)
-    bot.register_next_step_handler(message, get_day)
+    month = [str(i) for i in range(1, 13)]
+    markup.row(*month[:3])
+    markup.row(*month[3:6])
+    markup.row(*month[6:9])
+    markup.row(*month[9:])
+
+    bot.send_message(message.chat.id, "Виберіть місяць в яку вас цікавить розклад", reply_markup=markup)
+    bot.register_next_step_handler(message, get_month)
 
 
-def get_day(message):
-    week = 0 if message.text == 'Ця неділя' else 1
-    save_data('week', week)
-    markup = types.ReplyKeyboardMarkup()
-    markup.row(*list(DAYS.keys())[:3])
-    markup.row(*list(DAYS.keys())[3:6])
-    markup.row(list(DAYS.keys())[-1])
-    bot.send_message(message.chat.id, 'Виберіть день в який вас цікавить розклад', reply_markup=markup)
-    bot.register_next_step_handler(message, get_date)
-
-
-# def get_group(message):
-#     group = message.text
-#     save_data('group', group)
-#     markup = types.ReplyKeyboardMarkup()
-#     month = [str(i) for i in range(1, 13)]
-#     markup.row(*month[:3])
-#     markup.row(*month[3:6])
-#     markup.row(*month[6:9])
-#     markup.row(*month[9:])
-#
-#     bot.send_message(message.chat.id, "Виберіть місяць в яку вас цікавить розклад", reply_markup=markup)
-#     bot.register_next_step_handler(message, get_month)
-#
-#
-# def get_month(message):
-#     date = message.text
-#     save_data('date', date)
-#     markup = types.ReplyKeyboardMarkup()
-#     dates_range = [str(i) for i in range(1, 32)]
-#     markup.row(*dates_range[:7])
-#     markup.row(*dates_range[7:14])
-#     markup.row(*dates_range[14:21])
-#     markup.row(*dates_range[21: 28])
-#     markup.row(*dates_range[28:32])
-#     bot.send_message(message.chat.id, "Виберіть число в яке вас цікавить розклад", reply_markup=markup)
-#     bot.register_next_step_handler(message, get_date)
+def get_month(message):
+    if not validation_number(message) or not message.text in [str(i) for i in range(1, 13)]:
+        bot.send_message(message.chat.id, "Не коректнi данi. Повторiть спробу!")
+        bot.send_message(message.chat.id, "Виберіть місяць в яку вас цікавить розклад")
+        bot.register_next_step_handler(message, get_month)
+    else:
+        date = core_date(message.text)
+        save_data('date', date)
+        markup = types.ReplyKeyboardMarkup()
+        dates_range = [str(i) for i in range(1, 32)]
+        markup.row(*dates_range[:7])
+        markup.row(*dates_range[7:14])
+        markup.row(*dates_range[14:21])
+        markup.row(*dates_range[21: 28])
+        markup.row(*dates_range[28:32])
+        bot.send_message(message.chat.id, "Виберіть або введіть число в яке вас цікавить розклад", reply_markup=markup)
+        bot.register_next_step_handler(message, get_date)
 
 
 def get_date(message):
-    # date = f"{message.text}.{info.get('date', '')}"
-    day = DAYS[message.text]
-    save_data('day', day)
-    bot.send_message(message.chat.id, 'Ось розклад по вашому запиту')
-    show(message)
+    if not validation_number(message) or not message.text in [str(i) for i in range(1, 32)]:
+        bot.send_message(message.chat.id, "Не коректнi данi. Повторiть спробу!")
+        bot.send_message(message.chat.id, "Виберіть або введіть число в яке вас цікавить розклад")
+        bot.register_next_step_handler(message, get_date)
+    else:
+        day = core_date(message.text)
+        save_data('day', day)
+        bot.send_message(message.chat.id, 'Зачекайте, зараз пошукаю')
+        show(message)
 
 
 def show(message):
     """Вывод информации по введенным пользователем критериям"""
-    response = get_response(params=info)
-
-    [bot.send_message(message.chat.id,
-                      f"{el['lessonNumber']} УРОК -{el['auditoryNumber']}{el['lessonType']} -> {el['disciplineName']} <- {el['lecturer']}")
-     for el in response['curricularWeekList'][0].get('curricularDayList', 'ERROR')[4].get('lessonList', 'ERROR')]
-    markup = types.ReplyKeyboardMarkup()
-    markup.row('ТАК', 'НІ')
-    bot.send_message(message.chat.id, "Бажаєте переглянути даний розклад на іншій неділі ?", reply_markup=markup)
+    try:
+        response = get_timetable(params=info)
+        bot.send_message(message.chat.id, 'Ось результати по вашому запиту')
+        if len(response.get('lessonList', 'ERROR')) == 0:
+            bot.send_message(message.chat.id, "У цей день відсутні уроки (ваші дані коректні)")
+        [bot.send_message(message.chat.id,
+                          f"{el['lessonNumber']} УРОК -{el['auditoryNumber']} - {el['lessonType']} - {subgroup(el['subGroup'])} -> {el['disciplineName']} <- {el['lecturer']}")
+         for el in response.get('lessonList', 'ERROR')];
+        logging.info(f"Информация по данным параметрам была найдена")
+        markup = types.ReplyKeyboardMarkup()
+        markup.row('ТАК', 'НІ')
+        bot.send_message(message.chat.id, "Бажаєте переглянути даний розклад на іншу дату", reply_markup=markup)
+    except Exception as e:
+        logging.warning(f"Информация была не валидна \n {e}")
+        markup = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, "Ваші дані не валідні, повторіть спробу, для цього привітайтесь з ботом",
+                         reply_markup=markup)
 
 
 def save_data(key: str, value: str):
@@ -166,10 +196,34 @@ def save_data(key: str, value: str):
 
 def word_search(text: str) -> bool:
     """Функция котороя ищет слова приветсвия"""
-    key_word = ['hello', 'привет', 'hi', 'ку', 'вітаю', 'привіт']
+    key_word = ['hello', 'привет', 'hi', 'ку', 'вітаю', 'привіт', 'доброго', ]
     array = list(map(lambda el: el.lower(), text.split()))
     result = True in list(map(lambda el: el in array, key_word))
     return result
+
+
+def core_date(text: str) -> str:
+    """Добавлние в число первой цифры 0, в случае если оно не двуцифровое, для валидного представлнеия даты"""
+    if len(text) == 1: return f"0{text}"
+    return text
+
+
+def validation_number(message):
+    """Проверка является ли введенная информация целым числом или нет"""
+    try:
+        int(message.text)
+        return True
+    except Exception:
+        print(-1)
+        return False
+
+
+def subgroup(text):
+    """Перевод подгруппы в текст"""
+    if text == 0:
+        return "усі підгрупи"
+    else:
+        return f"{text} підгрупа"
 
 
 if __name__ == '__main__':
--- connect.py
+++ connect.py
@@ -1,14 +1,17 @@
 import requests
 
 
-def get_response(params: dict) -> dict:
+def get_timetable(params: dict) -> dict:
     headers = {
         'accept': '*/*',
         'Content-Type': 'application/json',
     }
 
-    data = f'"course":{params.get("course", 3)},"faculty":"{params.get("fac", "FCSA")}","group":"{params.get("group", "1ІСТ-18б")}"'
-    data = "{" + data + "}"
+    paramss = (
+        ('dateMonth', f'{params.get("day", "0")}-{params.get("date", "0")}'),
+    )
 
-    return requests.put('https://jetiq-curriculum-service.herokuapp.com:443/curriculum/convenient', headers=headers,
-                        data=data.encode('utf-8')).json()
+    data = "{" + f'"course":{params.get("course", 3)},"faculty":"{params.get("fac", "FCSA")}","group":"{params.get("group", "1ІСТ-18б")}"' + "}"
+
+    return requests.put('https://jetiq-curriculum-service.herokuapp.com:443/curriculum/byDate', headers=headers,
+                        params=paramss, data=data.encode('utf-8')).json()
